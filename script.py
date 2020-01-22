6cc38fa332e1c17e0a3644213e69088b1d6e132d
IA3270 <ianarcher@hotmail.com>
2020-01-22 21:31:49 +0000

    first commit

---
+++ script.py
@@ -0,0 +1,21 @@
+
+import sys
+import requests
+import os
+import math
+
+
+# print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+
+r = requests.get('https://www.thesun.co.uk/')
+print(r.status_code)
+
+# print(greet('World'))
+# print(greet('Ian'))
